e1ed29e9767e2b6c9de79337f262c773aa35c35b
TEONA <tekizashvili@gmail.com>
2022-10-16 18:47:21 +0400

    added javascript and readme files, project is finished

---
+++ script.js
@@ -0,0 +1,76 @@
+const viewsContainer = document.querySelector('span.pricing-card__views');
+const rangeInput = document.querySelector('input.pricing-card__views-range');
+const costContainer = document.querySelector('span.pricing-card__price');
+const priceFreqContainer = document.querySelector('span.pricing-card__billing-frequency');
+const frequencyToggle = document.querySelector('input#billing-freq');
+const form = document.querySelector('form.pricing-card');
+
+const VIEWS_DATA = [
+    {
+        pageViews: '10K',
+        monthlyCost: 8,
+        leftPercentage: 0
+    },
+    {
+        pageViews: '50K',
+        monthlyCost: 12,
+        leftPercentage: 25
+    },
+    {
+        pageViews: '100K',
+        monthlyCost: 16,
+        leftPercentage: 50
+    },
+    {
+        pageViews: '500K',
+        monthlyCost: 24,
+        leftPercentage: 75
+    },
+    {
+        pageViews: '1M',
+        monthlyCost: 36,
+        leftPercentage: 100
+    }
+];
+
+form.addEventListener('submit', (e) => e.preventDefault());
+
+const getData = () => {
+    const currentValue = rangeInput.value;
+    const index = currentValue - 1;
+    return { pageViews, monthlyCost, leftPercentage } = VIEWS_DATA[index];
+};
+
+const updatePageViews = () => {
+    const { pageViews } = getData();
+    viewsContainer.textContent = `${pageViews} PAGEVIEWS`;
+};
+
+const isAnnualFrequency = () => { return frequencyToggle.checked };
+
+const updatePriceFrequency = (isAnnual) => {
+    if (isAnnual) { priceFreqContainer.textContent = '/ year' }
+    else { priceFreqContainer.textContent = '/ month' };
+};
+
+const updateCost = () => {
+    const { monthlyCost } = getData();
+    const isAnnual = isAnnualFrequency();
+    const price = isAnnual ? ((monthlyCost * 12) * 0.75) : monthlyCost;
+    costContainer.textContent = `$${price.toFixed(2)}`;
+    updatePriceFrequency(isAnnual);
+};
+
+const updateLeftPercentage = () => {
+    const { leftPercentage } = getData();
+    form.style.setProperty('--left', leftPercentage);
+};
+
+const updateFormOnRangeInput = () => {
+    updatePageViews();
+    updateCost();
+    updateLeftPercentage();
+};
+
+rangeInput.addEventListener('input', updateFormOnRangeInput);
+frequencyToggle.addEventListener('input', updateCost);
